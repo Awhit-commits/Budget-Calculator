2e16fcabcb02845c47b7693e399000f63e0ddbf6
Awhit-commits <ndrw_whtmr@ymail.com>
2020-06-03 22:26:09 -0500

    Finished UI

--- budget-app/src/app/app.module.ts
+++ budget-app/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { AddItemComponent } from './add-item/add-item.component';
 import { BudgetItemListComponent } from './budget-item-list/budget-item-list.component';
+import { BudgetItemCardComponent } from './budget-item-list/budget-item-card/budget-item-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
     AddItemComponent,
-    BudgetItemListComponent
+    BudgetItemListComponent,
+    BudgetItemCardComponent
   ],
   imports: [
     BrowserModule,
